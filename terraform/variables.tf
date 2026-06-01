e59fd43b2756e498a2a2e6a2bb98d56b80767988
tolic <ivan081169@gmail.com>
2026-06-01 22:39:47 +0200

    Add Terraform resource group setup

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for the project"
+  type        = string
+  default     = "swedencentral"
+}
+
+variable "resource_group_name" {
+  description = "Main resource group name"
+  type        = string
+  default     = "rg-acs-project-itolic"
+}
+
+variable "common_tags" {
+  description = "Common tags required by the project"
+  type        = map(string)
+  default = {
+    university = "Algebra"
+    student    = "student@algebra.hr"
+  }
+}
